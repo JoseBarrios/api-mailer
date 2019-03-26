6969dd2bbd90fa5760542b1d490ede129c0c5b52
Jose Barrios <github@barrios.io>
2019-03-25 17:08:43 -0700

    Added test config that match ecosystem

--- test/aws.js
+++ test/aws.js
@@ -1,28 +1,20 @@
-var config = require('../config/aws-secret.json')
-var Mailer = require('../index.js');
-var mailer = new Mailer(config);
+const appInfo = require('../../config-oplab/application.json')
+const config = require('../../config-oplab/aws.json')
+const Mailer = require('../index.js');
+const mailer = new Mailer(config);
 
-let sender = {};
-sender.givenName ="SenderFirst";
-sender.familyName = "SenderLastName";
-sender.email = "jose@bevisible.soy";
-
-let recipient = {};
-recipient.givenName = "RecipientFirst"
-recipient.familyName = "RecipientLast"
-recipient.email = "success@simulator.amazonses.com" // 200
+//const recipient = {};
+//recipient.email = "success@simulator.amazonses.com" // 200
 //recipient.email = "bounce@simulator.amazonses.com"// 400
 //recipient.email = "ooto@simulator.amazonses.com"// Out of office
 //recipient.email = "complaint@simulator.amazonses.com"// Mark as Spam
 //recipient.email = "suppressionlist@simulator.amazonses.com"
 
-let email = {};
-email.sender = sender;
-email.recipient = recipient;
+const email = {};
+email.sender = config.sender;
+email.recipient = appInfo.administrator;
 email.about = "Mailer AWS Test";
 email.text = "<p style='color:green'> This is the Mailer (AWS) HTML content </p>"
-email.description = "This is the Mailer (AWS) text content"
-
 
 describe("Mailer AWS", function() {
 	it("#sendEmail", function(done) {
--- test/sendgrid.js
+++ test/sendgrid.js
@@ -1,28 +1,28 @@
-var config = require('../config/sendgrid-secret.json')
-var Mailer = require('../index.js');
-var mailer = new Mailer(config);
+/*var config = require('../config/sendgrid-secret.json')*/
+//var Mailer = require('../index.js');
+//var mailer = new Mailer(config);
 
-let recipient = {};
-recipient.givenName = "RecipientFirst"
-recipient.familyName = "RecipientLast"
-recipient.email = "jose@barrios.io"
+//let recipient = {};
+//recipient.givenName = "RecipientFirst"
+//recipient.familyName = "RecipientLast"
+//recipient.email = "jose@barrios.io"
 
-let sender = {};
-sender.givenName ="SenderFirst";
-sender.familyName = "SenderLastName";
-sender.email = "jose@bevisible.soy";
+//let sender = {};
+//sender.givenName ="SenderFirst";
+//sender.familyName = "SenderLastName";
+//sender.email = "jose@bevisible.soy";
 
-let email = {};
-email.sender = sender;
-email.recipient = recipient;
-email.about = "Subject line";
-email.text = "This is the Mailer (SendGrid) text content"
-email.messageAttachment = "<p style='color:green'> This is the Mailer (SendGrid) HTML content </p>"
+//let email = {};
+//email.sender = sender;
+//email.recipient = recipient;
+//email.about = "Subject line";
+//email.text = "This is the Mailer (SendGrid) text content"
+//email.messageAttachment = "<p style='color:green'> This is the Mailer (SendGrid) HTML content </p>"
 
-describe("Mailer SendGrid", function() {
-	it("#sendEmail", function(done) {
-		mailer.sendEmail(email)
-			.then(res  => { done(); })
-			.catch(err => { done(err); })
-	})
-})
+//describe("Mailer SendGrid", function() {
+	//it("#sendEmail", function(done) {
+		//mailer.sendEmail(email)
+			//.then(res  => { done(); })
+			//.catch(err => { done(err); })
+	//})
+/*})*/
